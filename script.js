01f743a8b657a3d60df86cbc811fecce4b303ca6
Sandeep Chakraborty <sandeepchakraborty20021224@gmail.com>
2023-09-12 19:49:20 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-}
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
 
 const sr = ScrollReveal({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
 });
 
-sr.reveal('.hero-text', {delay: 200, origin: 'top'});
-sr.reveal('.hero-img', {delay: 450, origin: 'top'});
-sr.reveal('.icons', {delay: 500, origin: 'left'});
-sr.reveal('.scroll-down', {delay: 500, origin: 'right'});
+sr.reveal(".hero-text", { delay: 200, origin: "top" });
+sr.reveal(".hero-img", { delay: 450, origin: "top" });
+
+// sr.reveal('.icons', {delay: 500, origin: 'left'});
+// sr.reveal('.scroll-down', {delay: 500, origin: 'right'});
+
+// Add effect
+const icons = document.querySelectorAll(".effect");
+
+const addClassWithDelay = (element, ClassName, delay) => {
+  setTimeout(() => {
+    element.classList.add(ClassName);
+  }, delay);
+};
+
+icons.forEach((element, index) => {
+  const className = "animateItems";
+  const delay = 100 * index;
+  addClassWithDelay(element, className, delay);
+});
